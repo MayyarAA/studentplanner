007aa576e60564a1c69981e60045a0c2aa31b5d6
MayyarAA <mayyar1235@gmail.com>
2020-10-25 00:11:13 -0400

    temp

--- taskDetails.php
+++ taskDetails.php
@@ -1,10 +1,18 @@
 <?php
 
 
-require("conn.php"); 
-session_start():
-$submitted_tastName= ''; 
-$submitted_taskDetail = ''; 
+
+
+// $submitted_tastName= ''; 
+// $submitted_taskDetail = ''; 
+
+$submitted_tastName = $_POST['taskTitle'];
+$submitted_taskDetail = $_POST['tastDecrip'];
+$temp = "4";
+echo "Hello";
+echo $submitted_tastName ;
+echo $temp;
+// header("location:$submitted_tastName");
 
 
 
