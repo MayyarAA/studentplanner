ce8c3ab46bce1ebe960623446cba61a0392363f3
Rolo123y <52054023+Rolo123y@users.noreply.github.com>
2020-10-25 00:22:43 -0400

    fixed bug with <form> not posting to .php

--- taskDetails.php
+++ taskDetails.php
@@ -1,20 +1,20 @@
 <?php
 
-
-
-
-// $submitted_tastName= ''; 
-// $submitted_taskDetail = ''; 
-
 $submitted_tastName = $_POST['taskTitle'];
 $submitted_taskDetail = $_POST['tastDecrip'];
-$temp = "4";
-echo "Hello";
-echo $submitted_tastName ;
-echo $temp;
-// header("location:$submitted_tastName");
 
+echo $submitted_tastName;
 
+$sql = "SELECT attraction FROM ".$tripType." WHERE city='".$city."';";
+$result = mysqli_query($conn, $sql_2);
+    
+$resulCheck = mysqli_num_rows($result);
+if ($resulCheck > 0) {
+	header("location: $submitted_tastName");
+} else {
+    echo "no records found";
+
+}
 
 
 
