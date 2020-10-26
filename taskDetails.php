f083425ba7a3665321378ed9ca0e2dabe41fd609
MayyarAA <mayyar1235@gmail.com>
2020-10-25 20:50:12 -0400

    added comments

--- taskDetails.php
+++ taskDetails.php
@@ -1,20 +1,19 @@
 <?php
 
+//declaring local php variables 
 $submitted_tastName = $_POST['taskTitle'];
 $submitted_taskDetail = $_POST['tastDecrip'];
 
-//echo $submitted_tastName;
+//binding php var to sql object
 echo $submitted_tastName;
+
+//imports the objects and valiues from conn
 require("conn.php"); 
 $sql = "INSERT INTO `task` (`taskID`, `taskTitle`, `description`, `dueDate`, `taskDateCreated`, `importance`, `typeOfWork`, `c.courseID`, `tl.listID`, `archived`) VALUES (null, '.$submitted_tastName.', '.$submitted_taskDetail.', '10', '10', '2', 'Hard', '111', '1', '1');";
+//executes the post call
 $result = mysqli_query($conn, $sql);
 echo $result;
-// $resulCheck = mysqli_num_rows($result);
-// if ($resulCheck > 0) {
-// 	echo "working sql";
-// } else {
-//     echo "no records found";
-// }
+
       
 
 
