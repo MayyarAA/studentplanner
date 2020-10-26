67f47724e10eaa7d953a37187d50748ea1acb8f9
MayyarAA <mayyar1235@gmail.com>
2020-10-25 20:37:48 -0400

    working full

--- taskDetails.php
+++ taskDetails.php
@@ -4,16 +4,21 @@ $submitted_tastName = $_POST['taskTitle'];
 $submitted_taskDetail = $_POST['tastDecrip'];
 
 //echo $submitted_tastName;
-
-
+echo $submitted_tastName;
 require("conn.php"); 
-$sql = "INSERT INTO `task` (`taskID`, `taskTitle`, `description`, `dueDate`, `taskDateCreated`, `importance`, `typeOfWork`, `c.courseID`, `tl.listID`, `archived`) VALUES (null, 'Temp', 'hard', '10', '10', '2', 'Hard', '111', '1', '1');";
+$sql = "INSERT INTO `task` (`taskID`, `taskTitle`, `description`, `dueDate`, `taskDateCreated`, `importance`, `typeOfWork`, `c.courseID`, `tl.listID`, `archived`) VALUES (null, '.$submitted_tastName.', '.$submitted_taskDetail.', '10', '10', '2', 'Hard', '111', '1', '1');";
 $result = mysqli_query($conn, $sql);
 echo $result;
-
+// $resulCheck = mysqli_num_rows($result);
+// if ($resulCheck > 0) {
+// 	echo "working sql";
+// } else {
+//     echo "no records found";
+// }
       
 
 
 
 ?>
+
 
