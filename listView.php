4c32b8d4b9f4bdc830ff0d7b5af5a4791e6187a4
Ryan <39310008+ryab0@users.noreply.github.com>
2020-11-07 21:01:18 -0500

    Open, View Details Delete Task
    Added opening task from list view, view task details in pop-in, delete task in pop-in

--- listView.php
+++ listView.php
@@ -1,3 +1,10 @@
+<?php
+//This is a workaround, where the page loads too fast before the delete function happen so we need to re-load after the fact.
+if ($_GET['r'] == t){
+header("Location: listView.php");
+die();
+}
+?>
 <!DOCTYPE html>
 <html lang="">
 
@@ -5,6 +12,7 @@
 <head>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
     <title>Student Planner</title>
+</head>
 
     <body>
         <h1 class="text-center">Student Planner</h1>
@@ -12,30 +20,108 @@
         <h2 class="text-center">List View</h2>
 	<br>
 
-    </body>
+    <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#createList">
+  Create List
+</button>
 <?php
 require("conn.php"); 
-
-
-// get complete details for a list
-$sql = "SELECT listID,listTitle,taskTitle, description FROM task NATURAL JOIN taskList ";
-$result = mysqli_query($conn, $sql);
-
-
-$data = array(); // create a variable to hold the information
-while ($row = mysqli_fetch_array($result)){
-  $data[] = $row; // add the row in to the results (data) array
+//If we have a delete function posted lets process it.
+if (!empty($_POST['deleteID'])){
+$stmt = $db->prepare('DELETE FROM task WHERE `taskID`=?');
+            $stmt->execute(array($_POST['deleteID']));       
+            $task = $stmt->fetch();
+}
+if (!empty($_POST['name'])){
+	//If the user submitted a new list request we run this code
+	$query = " 
+            INSERT INTO taskList ( 
+                listTitle, 
+                boardID
+            ) VALUES ( 
+                :listTitle, 
+                :boardID
+            ) 
+        "; 
+        $query_params = array( 
+            ':listTitle' => $_POST['name'], //Insert the user's input into the database
+            ':boardID' => 1 //Placeholder until we develop the board view function
+        ); 
+        $stmt = $db->prepare($query); 
+       	$result = $stmt->execute($query_params); 
 }
 
-// display list title - initially, there is just one list - this will change when multiple lists are implemented
-echo "<h3>" .$data[0][1]. " List</h3>";
-
-// traverse through and display the information of related tasks
-for ($x = 0; $x <= count($data); $x+=1) {
-    echo "Task ".$x.": " .$data[$x][2]."<br>";
-    echo "Description: " .$data[$x][3]."<br><br><br>";
-
-  }
-
+// get complete details for a list
+$stmt = $db->prepare('SELECT * FROM taskList ORDER BY `listID` ASC');
+            $stmt->execute();       
+            $lists = $stmt->fetchAll();
+echo "<div class='row'>";
+foreach ($lists as $list){
+	//This code is run FOR EACH list that exists.
+	echo "
+	<div class='col-xs-3'>
+	<div class='card'>
+	<div class='card-header'>".$list['listTitle']."</div>
+	<ul class='list-group list-group-flush'>
+	";
+  
+    
+	$stmt = $db->prepare('SELECT * FROM task WHERE `tl.listID` = ?');
+            $stmt->execute(array($list['listID']));       
+            $tasks = $stmt->fetchAll();
+            foreach ($tasks as $task){
+            	//This code is run FOR EACH task in each list. Here we output an html row with some data. It also lets it target a custom modal to pop up each viewTask
+            	echo "<li class='list-group-item' onclick='dynamicModal(".$task['taskID'].")' data-toggle='modal' data-target='#viewTask'>".$task['taskTitle'].": ".$task['description']."</li>";
+            }
+    echo " 
+    </ul>
+	</div>
+	</div>";
+}
+echo "</div>";
+//Below we have the code for our "modal" which pops up when the user clicks the add new list button. It is a simple form that submits back here for a "page refresh" with the new list.
 ?>
-</head>
+<div class="modal fade" id="createList" tabindex="-1" aria-labelledby="createList" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Add New List</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <form action="" class="form-newList" method="POST">
+
+            <label for="inputName" class="sr-only">List Name</label>
+            <input type="text" name="name" value="" class="form-control" placeholder="List Name" required/>
+            <button class="btn btn-lg btn-primary btn-block" type="submit" value="Register">Create</button>
+        </form>
+      </div>
+    </div>
+  </div>
+</div>
+<div class="modal fade" id="viewTask" tabindex="-1" aria-labelledby="viewTask" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Task view</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <iframe sandbox="allow-top-navigation allow-scripts allow-forms" class="embed-responsive-item" id="viewTaskFrame" style="border:0; width:458px; height:500px;" src="sample.com"></iframe>
+      </div>
+    </div>
+  </div>
+</div>
+<script>
+function dynamicModal(str)
+{
+$("#viewTaskFrame").attr("src", "https://mansci-db.uwaterloo.ca/~wmmeyer/wmmeyer/viewTask.php?id="+str);
+}
+</script>
+<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+</body>
---
+++ viewTask.php
@@ -0,0 +1,58 @@
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+<table class="table">
+  <tbody>
+  <?php
+  //This document is the modal popup for viewtask, we needed to have the individual page which uses GET to know which task it needs to retrieve. It is viewed in an iframe in listView
+ require('conn.php');
+
+ $stmt = $db->prepare('SELECT * FROM task t
+INNER JOIN course c
+ON t.`c.courseID` = c.courseID 
+INNER JOIN taskList tl
+ON t.`tl.listID` = tl.listID WHERE `taskID`=?');
+            $stmt->execute(array($_GET['id']));       
+            $task = $stmt->fetch();
+            //Now we have our task info in the $task variable, lets output the information for the user.
+            ?>
+    <tr>
+      <th>ID</th>
+      <td><?php echo $task['taskID'];?></td>
+    </tr>
+    <tr>
+      <th scope="row">Title</th>
+      <td><?php echo $task['taskTitle'];?></td>
+    </tr>
+    <tr>
+      <th scope="row">Description</th>
+      <td><?php echo $task['description'];?></td>
+    </tr>
+    <tr>
+      <th scope="row">Due Date</th>
+      <td><?php echo date("Y D M j G:i:s", $task['dueDate']);?></td>
+    </tr>
+    <tr>
+      <th scope="row">Date Created</th>
+      <td><?php echo date("Y D M j G:i:s", $task['taskDateCreated']);?></td>
+    </tr>
+    <tr>
+      <th scope="row">Importance</th>
+      <td><?php echo $task['importance'];?></td>
+    </tr>
+    <tr>
+      <th scope="row">Type Of Work</th>
+      <td><?php echo $task['typeOfWork'];?></td>
+    </tr>
+    <tr>
+      <th scope="row">Course</th>
+      <td><?php echo $task['courseTitle'];?></td>
+    </tr>
+    <tr>
+      <th scope="row">List</th>
+      <td><?php echo $task['listTitle'];?></td>
+    </tr>
+  </tbody>
+</table>
+<form action="listView.php" onclick="window.top.location = 'https://mansci-db.uwaterloo.ca/~wmmeyer/wmmeyer/listView.php?r=t'" class="form-newList" method="POST">
+ <input type="hidden" id="deleteID" name="deleteID" value="<?php echo $_GET['id']; ?>">
+<input type="submit" class="btn btn-danger" name="delete" value="Delete Task"></input>
+</form>
