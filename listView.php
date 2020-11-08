c0334d57e148f972459cf2d78e3fefb92c1882ad
Rolo123y <52054023+Rolo123y@users.noreply.github.com>
2020-11-08 03:26:35 -0500

    simple UI/UX changes + adding trashcan icon on hover of lists

--- listView.php
+++ listView.php
@@ -4,35 +4,38 @@ if ($_GET['r'] == t){
 header("Location: listView.php");
 die();
 }
+
 ?>
 <!DOCTYPE html>
 <html lang="">
 
-
 <head>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <link href="CSS/jumbotron" rel="stylesheet">
+    <link href="CSS/BoardView-Body.css" rel="stylesheet">
     <title>Student Planner</title>
+
 </head>
 
     <body>
-        <h1 class="text-center">Student Planner</h1>
-	<br>
-        <h2 class="text-center">List View</h2>
-	<br>
+    <div class="jumbotron">
+        <div class="container">
+            <h1 class="display-3">BoardView</h1>
+        </div>
+    </div>
+    <button type="button" class="btn btn-primary" style='margin-left: 5%;' data-toggle="modal" data-target="#createList">Create List</button>
 
-    <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#createList">
-  Create List
-</button>
 <?php
 require("conn.php"); 
+
 //If we have a delete function posted lets process it.
 if (!empty($_POST['deleteID'])){
 $stmt = $db->prepare('DELETE FROM task WHERE `taskID`=?');
             $stmt->execute(array($_POST['deleteID']));       
             $task = $stmt->fetch();
 }
+//If the user submitted a new list request we run this code
 if (!empty($_POST['name'])){
-	//If the user submitted a new list request we run this code
 	$query = " 
             INSERT INTO taskList ( 
                 listTitle, 
@@ -49,6 +52,19 @@ if (!empty($_POST['name'])){
         $stmt = $db->prepare($query); 
        	$result = $stmt->execute($query_params); 
 }
+// If the user wants to delete a list, they need to confirm first then we can run this code to delete the list
+// if(!empty($_POST['Yes'])){
+//   $query= "
+//           DELETE FROM taskList
+//           where listTitle= :listTitle and boardID = :boardID
+//       ";
+//   $query_params = array(
+//     ':listTitle' => $_POST[''],
+//     ':boardID' => 1
+//   );
+//   $stmt = $db->prepare($query);
+//   $result = $stmt->execute($query_params);
+// }
 
 // get complete details for a list
 $stmt = $db->prepare('SELECT * FROM taskList ORDER BY `listID` ASC');
@@ -56,15 +72,22 @@ $stmt = $db->prepare('SELECT * FROM taskList ORDER BY `listID` ASC');
             $lists = $stmt->fetchAll();
 echo "<div class='row'>";
 foreach ($lists as $list){
-	//This code is run FOR EACH list that exists.
+  //This code is run FOR EACH list that exists.
 	echo "
 	<div class='col-xs-3'>
 	<div class='card'>
-	<div class='card-header'>".$list['listTitle']."</div>
+	<div class='card-header'>
+    <div class ='card-Title'>
+      ".$list['listTitle']."
+        <button type='button' class='btn btn-light'>
+          <img src='imgs/svg/trash.svg' style='width:20 height:20'></img>
+        </button>   
+    </div>
+  </div>
+
 	<ul class='list-group list-group-flush'>
 	";
   
-    
 	$stmt = $db->prepare('SELECT * FROM task WHERE `tl.listID` = ?');
             $stmt->execute(array($list['listID']));       
             $tasks = $stmt->fetchAll();
@@ -91,7 +114,6 @@ echo "</div>";
       </div>
       <div class="modal-body">
         <form action="" class="form-newList" method="POST">
-
             <label for="inputName" class="sr-only">List Name</label>
             <input type="text" name="name" value="" class="form-control" placeholder="List Name" required/>
             <button class="btn btn-lg btn-primary btn-block" type="submit" value="Register">Create</button>
@@ -100,6 +122,29 @@ echo "</div>";
     </div>
   </div>
 </div>
+<!-- popup to make sure if user wants to delete the task list -->
+<!-- <div class="modal fade" id="DeleteList" tabindex="-1" aria-labelledby="DeleteList" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="DeleteListTitle">Delete <?php echo $_GET['Title'];
+        
+        ?></h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <form action="" class="form-newList" method="POST">
+          <p>Are you sure you want to delete this list?</p>
+          <button class="btn btn-lg btn-primary btn-block" type="submit" name="Yes" value="Yes">Yes</button>
+          <button class="btn btn-lg btn-primary btn-block" type="submit" name="No" value="No">No</button>
+        </form>
+      </div>
+    </div>
+  </div>
+</div> -->
+<!-- popup to view task details -->
 <div class="modal fade" id="viewTask" tabindex="-1" aria-labelledby="viewTask" aria-hidden="true">
   <div class="modal-dialog">
     <div class="modal-content">
@@ -118,9 +163,18 @@ echo "</div>";
 <script>
 function dynamicModal(str)
 {
-$("#viewTaskFrame").attr("src", "https://mansci-db.uwaterloo.ca/~wmmeyer/wmmeyer/viewTask.php?id="+str);
+$("#viewTaskFrame").attr("src", "https://mansci-db.uwaterloo.ca/~wmmeyer/r2/studentplanner/viewTask.php?id="+str);
 }
 </script>
+
+<!-- <script>
+function DeleteModel(str)
+{
+$("#DeleteListTitle").attr("src", "https://mansci-db.uwaterloo.ca/~wmmeyer/r2/studentplanner/viewTask.php?Title="+str);
+}
+</script> -->
+
+
 <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
